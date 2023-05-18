a91ed548c21397021856c220c2db3bfb1aa16658
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-05-19 00:04:26 +0600

    Added all toys

--- index.js
+++ index.js
@@ -27,8 +27,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     client.connect();
 
-
+    const toysCollection = client.db("battleZoneToys").collection("allToys")
     
+    app.get("/allToys", async(req,res)=>{
+        const result = await toysCollection.find().toArray()
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
