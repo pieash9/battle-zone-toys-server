10aedf06ecae5f58808f59f33c293993aab04d40
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-05-19 18:36:26 +0600

    Search implemented

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const app = express()
-const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //middleware
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fiktc6e.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,16 +25,53 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     client.connect();
 
-    const toysCollection = client.db("battleZoneToys").collection("allToys")
-    
-    app.get("/allToys", async(req,res)=>{
-        const result = await toysCollection.find().toArray()
-        res.send(result)
-    })
+    const toysCollection = client.db("battleZoneToys").collection("allToys");
+
+    const indexKeys = { title: 1 };
+    const indexOptions = { name: "ToyName" };
+    const result = await toysCollection.createIndex(indexKeys, indexOptions);
+
+    app.get("/searchByToyName/:text", async (req, res) => {
+      const searchText = req.params.text;
+      const result = await toysCollection
+        .find({
+          $or: [
+            {
+              name: { $regex: searchText, $options: "i" },
+            },
+          ],
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    // get all data
+    app.get("/allToys", async (req, res) => {
+      const result = await toysCollection.find().toArray();
+      res.send(result);
+    });
+
+    //get data by category
+    app.get("/allToys/:category", async (req, res) => {
+      const category = req.params.category;
+      let result;
+      if (
+        category == "marvel" ||
+        category == "starWars" ||
+        category == "avengers"
+      ) {
+        result = await toysCollection.find({ subCategory: category }).toArray();
+      } else {
+        result = await toysCollection.find({}).toArray();
+      }
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -44,11 +79,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/',(req,res)=>{
-    res.send("Battle Zone Toys server is running")
-})
-app.listen(port,()=>{
-    console.log("Battle Zone Toys server is running at",port)
-})
+app.get("/", (req, res) => {
+  res.send("Battle Zone Toys server is running");
+});
+app.listen(port, () => {
+  console.log("Battle Zone Toys server is running at", port);
+});
