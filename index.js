908b0c62673105f53efc0822d44077917595153c
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-05-21 09:50:18 +0600

    added config

--- index.js
+++ index.js
@@ -23,33 +23,40 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    client.connect();
+    // client.connect();
 
     const toysCollection = client.db("battleZoneToys").collection("allToys");
 
-    const indexKeys = { title: 1 };
-    const indexOptions = { name: "ToyName" };
-    const result = await toysCollection.createIndex(indexKeys, indexOptions);
+    // const indexKeys = { title: 1 };
+    // const indexOptions = { name: "ToyName" };
+    // const result = await toysCollection.createIndex(indexKeys, indexOptions);
 
-
-    // search by toy name
+    // search all data by Toy name
     app.get("/searchByToyName/:text", async (req, res) => {
       const searchText = req.params.text;
-      const result = await toysCollection
-        .find({
-          $or: [
-            {
-              name: { $regex: searchText, $options: "i" },
-            },
-          ],
-        })
-        .limit(20).toArray();
-      res.send(result);
+      if (searchText) {
+        const result = await toysCollection
+          .find({
+            $or: [
+              {
+                name: { $regex: searchText, $options: "i" },
+              },
+            ],
+          })
+          .limit(20)
+          .toArray();
+        res.send(result);
+      } else {
+        res
+          .status(404)
+          .send({ error: true, message: "Please do a valid query" });
+      }
     });
 
     // get all data
     app.get("/allToys", async (req, res) => {
       //using limit
+      // sorting
       const result = await toysCollection.find().limit(20).toArray();
       res.send(result);
     });
@@ -58,8 +65,19 @@ async function run() {
     app.get("/allToys/details/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      //using limit
-      const result = await toysCollection.findOne(query)
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    });
+
+    //get data by email/user (My data)
+    app.get("/myToys/:email", async (req, res) => {
+      const email = req.params.email;
+      const sortedBy = req.query.sort;
+      const result = await toysCollection
+        .find({ sellerEmail: email })
+        .sort({ createdAt: sortedBy })
+        .limit(20)
+        .toArray();
       res.send(result);
     });
 
@@ -76,6 +94,38 @@ async function run() {
       } else {
         result = await toysCollection.find({}).toArray();
       }
+      res.send(result);
+    });
+
+    //Post a toy
+    app.post("/addAToy", async (req, res) => {
+      const body = req.body;
+      // console.log(body);
+      const result = await toysCollection.insertOne(body);
+      res.send(result);
+    });
+
+    //update a toy
+    app.put("/updateToy/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedToyData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedToy = {
+        $set: {
+          price: updatedToyData.price,
+          availableQuantity: updatedToyData.availableQuantity,
+          detailDescription: updatedToyData.detailDescription,
+        },
+      };
+      const result = await toysCollection.updateOne(filter, updatedToy);
+      res.send(result);
+    });
+
+    //delete a toy
+    app.delete("/removeAToy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.deleteOne(query);
       res.send(result);
     });
 
