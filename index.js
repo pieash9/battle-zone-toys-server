fdb9f5b3cc60d3a821de4acec2dacda0e8119759
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-05-21 18:20:42 +0600

    quick fix

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
+    
     // await client.close();
   }
 }
